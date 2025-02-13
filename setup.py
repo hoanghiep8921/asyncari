2a4bbf3c655618c39cf68c58b623670381ecf27e
hiepdh <hiepdh@vnpay.vn>
2025-02-13 16:30:45 +0700

    add logic skip stasis and dial agent

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
         "setuptools_scm",
     ],
     install_requires=[
-        "anyio >= 4",
+        # "anyio >= 4",
         "asyncswagger11",
-        "asks >= 2.2",
+        # "asks >= 2.2",
         "attrs >= 18",
         "asyncwebsockets",
     ],
